8c65af1d4a9d99ccbf99a28af60aef79b57fe217
Alfafis <ctpmmg2009_101@hotmail.com>
2022-11-14 20:00:26 -0300

    create: repository

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <a href="https://github.com/Alfafis/React-Vanilla-Preset">
+      React Vanilla Preset
+    </a>
+  );
+};
+
+export default App;
